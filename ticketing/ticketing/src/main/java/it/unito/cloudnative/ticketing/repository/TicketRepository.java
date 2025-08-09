43358038937c06ead251a5b751bbf89060dc5232
MariSeest <seest97@gmail.com>
2025-08-09 03:44:22 +0200

    Delete ticketing/ticketing directory

--- ticketing/ticketing/src/main/java/it/unito/cloudnative/ticketing/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package it.unito.cloudnative.ticketing.repository;
-
-import it.unito.cloudnative.ticketing.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
