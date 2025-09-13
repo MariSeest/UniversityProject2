59f2bcc41d7cb1d448c740dedbb38abda030a98c
LuisaMele <maria.mele679@edu.unito.it>
2025-09-13 19:51:02 +0200

    commit LOCALHOST TO RENDER

--- ticketing-frontend/src/pages/TicketDetails.jsx
+++ ticketing-frontend/src/pages/TicketDetails.jsx
@@ -7,7 +7,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const API_BASE_URL =
     (import.meta.env.VITE_API_URL || '').replace(/\/$/, '') ||
-    'https://universityproject2.onrender.com/'
+    'https://berepositoryuniversityprojectjava.onrender.com'
 
 
 const api = axios.create({
